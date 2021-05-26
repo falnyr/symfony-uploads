76ae7ec20a8496b5366d5437758b6dd8ab2eae14
Jan Richter <falnyr@gmail.com>
2021-05-27 11:58:10 +1200

    feat: file upload fixture

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,8 +5,11 @@ namespace App\DataFixtures;
 use App\Entity\Article;
 use App\Entity\Comment;
 use App\Entity\Tag;
+use App\Service\UploaderHelper;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\File;
 
 class ArticleFixtures extends BaseFixture implements DependentFixtureInterface
 {
@@ -21,6 +24,16 @@ class ArticleFixtures extends BaseFixture implements DependentFixtureInterface
         'mercury.jpeg',
         'lightspeed.png',
     ];
+    /**
+     * @var UploaderHelper
+     */
+    private $uploaderHelper;
+
+    public function __construct(UploaderHelper $uploaderHelper)
+    {
+        $this->uploaderHelper = $uploaderHelper;
+
+    }
 
     protected function loadData(ObjectManager $manager)
     {
@@ -52,9 +65,11 @@ EOF
                 $article->setPublishedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
             }
 
+            $imageFilename = $this->fakeUploadImage();
+
             $article->setAuthor($this->getRandomReference('main_users'))
                 ->setHeartCount($this->faker->numberBetween(5, 100))
-                ->setImageFilename($this->faker->randomElement(self::$articleImages))
+                ->setImageFilename($imageFilename)
             ;
 
             $tags = $this->getRandomReferences('main_tags', $this->faker->numberBetween(0, 5));
@@ -74,5 +89,18 @@ EOF
             TagFixture::class,
             UserFixture::class,
         ];
+    }
+
+    /**
+     * @return string
+     */
+    private function fakeUploadImage(): string
+    {
+        $randomImage = $this->faker->randomElement(self::$articleImages);
+        $fs = new Filesystem();
+        $targetPath = sys_get_temp_dir() . '/' . $randomImage;
+        $fs->copy(__DIR__ . '/images/' . $randomImage, $targetPath, true);
+
+        return $this->uploaderHelper->uploadArticleImage(new File($targetPath));
     }
 }
