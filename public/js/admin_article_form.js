b379c0230e43476336de6d7c407e6c1216be450f
Jan Richter <falnyr@gmail.com>
2021-05-31 13:57:06 +1200

    feat: rendering uploads realtime

--- public/js/admin_article_form.js
+++ public/js/admin_article_form.js
@@ -2,7 +2,8 @@ Dropzone.autoDiscover = false;
 
 $(document).ready(function() {
 
-    initializeDropzone();
+    var referenceList = new ReferenceList($('.js-reference-list'));
+    initializeDropzone(referenceList);
 
     var $locationSelect = $('.js-article-form-location');
     var $specificLocationTarget = $('.js-specific-location-target');
@@ -30,7 +31,46 @@ $(document).ready(function() {
     });
 });
 
-function initializeDropzone() {
+class ReferenceList
+{
+    constructor($element) {
+        this.$element = $element;
+        this.references = [];
+        this.render();
+        $.ajax({
+            url: this.$element.data('url')
+        }).then(data => {
+            this.references = data;
+            this.render();
+        })
+    }
+
+    addReference(reference) {
+        this.references.push(reference);
+        this.render();
+    }
+
+    render() {
+        const itemsHtml = this.references.map(reference => {
+            return `
+<li class="list-group-item d-flex justify-content-between align-items-center">
+    ${reference.originalFilename}
+    <span>
+        <a href="/admin/article/references/${reference.id}/download"><span class="fa fa-download"></span></a>
+    </span>
+</li>
+`
+        });
+        this.$element.html(itemsHtml.join(''));
+    }
+}
+
+/**
+ *
+ * @param {ReferenceList} referenceList
+ */
+function initializeDropzone(referenceList) {
+
     var formElement = document.querySelector('.js-reference-dropzone');
     if ( ! formElement) {
         return;
@@ -39,6 +79,9 @@ function initializeDropzone() {
     var dropzone = new Dropzone(formElement, {
         paramName: 'reference',
         init: function() {
+            this.on('success', function(file, data) {
+                referenceList.addReference(data)
+            });
             this.on('error', function(file, data) {
                if (data.detail) {
                    this.emit('error', file, data.detail)
