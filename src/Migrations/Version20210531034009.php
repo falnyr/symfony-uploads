77c9b98195e1e2e41190495782d13ef5dc5ceb78
Jan Richter <falnyr@gmail.com>
2021-05-31 15:48:20 +1200

    feat: sortable

---
+++ src/Migrations/Version20210531034009.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210531034009 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE article_reference ADD position INT NOT NULL');
+        $this->addSql('ALTER TABLE article_reference RENAME INDEX idx_15232a177294869c TO IDX_749619377294869C');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE article_reference DROP position');
+        $this->addSql('ALTER TABLE article_reference RENAME INDEX idx_749619377294869c TO IDX_15232A177294869C');
+    }
+}
